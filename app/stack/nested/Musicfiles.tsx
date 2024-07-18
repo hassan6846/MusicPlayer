e600761d4ab3e953aa71a6ba4ff9d90544f13777
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-07-18 21:22:50 +0500

    main

---
+++ app/stack/nested/Musicfiles.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Musicfiles = () => {
+  return (
+    <View>
+      <Text>Musicfiles</Text>
+    </View>
+  )
+}
+
+export default Musicfiles
